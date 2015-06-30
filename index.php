8d5b5843c3a12fb6c77084e69b4eb6f977a4d174
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2015-06-30 22:40:38 +0200

    go go go

--- index.php
+++ index.php
@@ -34,7 +34,15 @@
 			En las últimas décadas las bibliotecas europeas han dado un paso de gigante  con la digitalización masiva de sus fondos históricos y la apertura de sus contenidos para el uso y disfrute de la sociedad digital global. Sin embargo, investigadores y profesores experimentan grandes dificultades a la hora de utilizar, enriquecer y compartir esos contenidos entre sí y con sus alumnos. Nuestro proyecto pretende explorar las nuevas necesidades que tienen los usuarios de bibliotecas, bases de datos y repositorios digitales europeos con el objetivo de hacer evolucionar el modelo digital tradicional hacia la Smartlibrary (Dolores Romero López: 2014) que propone la compilación, integración y descarga de contenidos según las necesidades de los usuarios con el fin de enriquecer los usos de nuestro pasado y desarrollar nuevos recursos útiles para la docencia y la investigación.
 		</p>
 	</div>
-	 
+	<hr class="linea_horizontal">
+	<div class="colecciones_smart">
+		
+		<p class="colecciones_titulo"> Colecciones Actuales<p>
+		
+		<div class="colecciones_smart_list">
+		<a class="colecciones_smart_library" href="http://repositorios.fdi.ucm.es/mnemosine/">Mnemosine</a>
+		</div>
+	</div> 
 	<script>
 	  var nav = responsiveNav(".nav-collapse");
 	</script>
