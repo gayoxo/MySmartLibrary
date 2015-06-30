56f4e778408cfb8530e9bd269c5ec92eab58c024
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2015-06-30 22:23:33 +0200

    inicial

---
+++ cabecera.php
@@ -0,0 +1,11 @@
+  <div class="cabecera">
+<!--	<div class="cabecera_smart_library">
+		<img src="imagenes/smart_library_icon.png" width="50" height="50" border="0" alt="smart_library_icon">
+	</div> -->
+	  
+	<a href="<?php echo $LinkAtras; ?>"> 
+			<img src="imagenes/red.png" width="300" height="300" class="Bannericon" alt=""> 
+			 
+		</a>	
+	</div>
+	<!--<span class="texto_cabecera">Biblioteca Digital de la Otra Edad de Plata</span> -->
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <title>Mnemosine</title>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" href="responsive-nav.css">
+	<script src="responsive-nav.js"></script>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="nhpup_1.1.js"></script>
+  </head>
+  <body>
+  
+  
+  <div class="wrapper">
+	<div class="zona_icono">
+	
+	
+	<?php include "cabecera.php";?>
+	
+     </div>
+	 
+	 <p class="mysmartlibraryproyect_texto_Cabecera">¿Que es el proyecto My Smart Library?</p>
+	<hr class="linea_horizontal">
+	<div class="smart_library_body">
+		<p class="justifiedtext">
+		MySmartLibrary es nuevo modelo de biblioteca digital que permite la importación, gracias al desarrollo de la herramienta Clavy, de los metadatos procedentes de las grandes bibliotecas, su adaptación a las necesidades específicas de los usuarios, el enriquecimiento de los objetos con herramientas digitales y la transferencia de los objetos enriquecidos a la comunidad global.
+		</p>
+	<div class="imagen_centrada">
+		<img class="icono_entre_parrafos" src="imagenes/red.png" width="200" height="200" alt="">
+	</div>
+		<p class="justifiedtext">
+			En las últimas décadas las bibliotecas europeas han dado un paso de gigante  con la digitalización masiva de sus fondos históricos y la apertura de sus contenidos para el uso y disfrute de la sociedad digital global. Sin embargo, investigadores y profesores experimentan grandes dificultades a la hora de utilizar, enriquecer y compartir esos contenidos entre sí y con sus alumnos. Nuestro proyecto pretende explorar las nuevas necesidades que tienen los usuarios de bibliotecas, bases de datos y repositorios digitales europeos con el objetivo de hacer evolucionar el modelo digital tradicional hacia la Smartlibrary (Dolores Romero López: 2014) que propone la compilación, integración y descarga de contenidos según las necesidades de los usuarios con el fin de enriquecer los usos de nuestro pasado y desarrollar nuevos recursos útiles para la docencia y la investigación.
+		</p>
+	</div>
+	 
+	<script>
+	  var nav = responsiveNav(".nav-collapse");
+	</script>
+  </body>
+</html>
