c4e7c049932bb40098dc29101f35c231e5a8f3a9
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2015-07-02 11:02:22 +0200

    gggg

--- index.php
+++ index.php
@@ -24,15 +24,26 @@
 	 <p class="mysmartlibraryproyect_texto_Cabecera">¿Que es el proyecto My Smart Library?</p>
 	<hr class="linea_horizontal">
 	<div class="smart_library_body">
-		<p class="justifiedtext">
-		MySmartLibrary es nuevo modelo de biblioteca digital que permite la importación, gracias al desarrollo de la herramienta Clavy, de los metadatos procedentes de las grandes bibliotecas, su adaptación a las necesidades específicas de los usuarios, el enriquecimiento de los objetos con herramientas digitales y la transferencia de los objetos enriquecidos a la comunidad global.
+	
+		<div class="grupo">
+		<img class="icono_en_grupo" src="imagenes/red.png" alt=""><br>
+		<h1 class="titulo">Definición</h1>
+		<p class="justifiedtext_engrupo">
+		MySmartLibrary es nuevo modelo de biblioteca digital que permite la importación, gracias al desarrollo de la herramienta Clavy, de los metadatos procedentes de las grandes bibliotecas, su adaptación a las necesidades específicas de los usuarios, el enriquecimiento de los objetos con herramientas digitales y la transferencia de los objetos enriquecidos a la comunidad global.		</p>
+		</div>
+		<div class="grupo">
+		<img class="icono_en_grupo" src="imagenes/red.png" alt=""><br>
+		<h1 class="titulo">Proyecto</h1>
+		<p class="justifiedtext_engrupo">
+		En las últimas décadas las bibliotecas europeas han dado un paso de gigante  con la digitalización masiva de sus fondos históricos y la apertura de sus contenidos para el uso y disfrute de la sociedad digital global. Sin embargo, investigadores y profesores experimentan grandes dificultades a la hora de utilizar, enriquecer y compartir esos contenidos entre sí y con sus alumnos. Nuestro proyecto pretende explorar las nuevas necesidades que tienen los usuarios de bibliotecas, bases de datos y repositorios digitales europeos con el objetivo de hacer evolucionar el modelo digital tradicional hacia la Smartlibrary (Dolores Romero López: 2014) que propone la compilación, integración y descarga de contenidos según las necesidades de los usuarios con el fin de enriquecer los usos de nuestro pasado y desarrollar nuevos recursos útiles para la docencia y la investigación.		</p>
+		</div>
+		<div class="grupo">
+		<img class="icono_en_grupo" src="imagenes/red.png" alt=""><br>
+		<h1 class="titulo">Start-up</h1>
+		<p class="justifiedtext_engrupo">
+		Se presentará un modelo de start-up próximamente.
 		</p>
-	<div class="imagen_centrada">
-		<img class="icono_entre_parrafos" src="imagenes/red.png" width="200" height="200" alt="">
-	</div>
-		<p class="justifiedtext">
-			En las últimas décadas las bibliotecas europeas han dado un paso de gigante  con la digitalización masiva de sus fondos históricos y la apertura de sus contenidos para el uso y disfrute de la sociedad digital global. Sin embargo, investigadores y profesores experimentan grandes dificultades a la hora de utilizar, enriquecer y compartir esos contenidos entre sí y con sus alumnos. Nuestro proyecto pretende explorar las nuevas necesidades que tienen los usuarios de bibliotecas, bases de datos y repositorios digitales europeos con el objetivo de hacer evolucionar el modelo digital tradicional hacia la Smartlibrary (Dolores Romero López: 2014) que propone la compilación, integración y descarga de contenidos según las necesidades de los usuarios con el fin de enriquecer los usos de nuestro pasado y desarrollar nuevos recursos útiles para la docencia y la investigación.
-		</p>
+		</div>
 	</div>
 	<hr class="linea_horizontal">
 	<div class="colecciones_smart">
@@ -40,7 +51,12 @@
 		<p class="colecciones_titulo"> Colecciones Actuales<p>
 		
 		<div class="colecciones_smart_list">
-		<a class="colecciones_smart_library" href="http://repositorios.fdi.ucm.es/mnemosine/" target="_blank">Mnemosine</a>
+			<div class="colecciones_smart_list_elemnt">
+				<a class="colecciones_smart_library" href="http://repositorios.fdi.ucm.es/mnemosine/" target="_blank">
+				<img class="icono_coleccion" src="imagenes/logomenu.png" alt="">
+				<br>Mnemosine
+				</a>
+		</div>
 		</div>
 	</div> 
 	<script>
